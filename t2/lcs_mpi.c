0eb105a6a48456ed29c04006666a6ed6cd9e453e
glo18 <glaide.lara@gmail.com>
2022-09-08 13:33:31 -0300

    ajeitando funcoes

--- t2/lcs_mpi.c
+++ t2/lcs_mpi.c
@@ -13,13 +13,13 @@ char *string_B;
 char *unique_chars_C; // unique alphabets
 int c_len;
 int *P_Matrix;
-int *DP_Results; // to store the DP values
-int *dp_prev_row;
+int *atual; // to store the DP values
+int *anterior;
 // function prototypes
 int get_index_of_character(char *str, char x, int len);
 void print_matrix(int **x, int row, int col);
-void calc_P_matrix_v2(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size);
-int lcs_yang_v2(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size);
+void calc_matriz_dist(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size, int resto_p, int num_procs);
+int calc_lcs(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size);
 int lcs(int **DP, char *A, char *B, int m, int n);
 
 int get_index_of_character(char *str, char x, int len)
@@ -46,7 +46,7 @@ void print_matrix(int **x, int row, int col)
     }
 }
 
-void calc_P_matrix_v2(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size)
+void calc_matriz_dist(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size, int resto_p, int num_procs)
 {
     char receive_array_for_scatter_c[chunk_size];
     int receive_array_for_scatter_p[chunk_size * (len_b + 1)];
@@ -75,7 +75,7 @@ void calc_P_matrix_v2(int *P, char *b, int len_b, char *c, int len_c, int myrank
     MPI_Gather(receive_array_for_scatter_p, chunk_size * (len_b + 1), MPI_INT, P, chunk_size * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
 }
 
-int lcs_yang_v2(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size)
+int calc_lcs(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size)
 {
     MPI_Bcast(P, (u * (n + 1)), MPI_INT, 0, MPI_COMM_WORLD);
     for (int i = 1; i < m + 1; i++)
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
 
     int my_rank;
     int num_procs;
-    int chunk_size_p, chunk_size_dp; // chunk_size for P matrix and DP matrix
+    int chunk_size_p, chunk_size_dp, resto_p; // chunk_size for P matrix and DP matrix
     int res;
 
     MPI_Init(&argc, &argv);
@@ -154,13 +154,14 @@ int main(int argc, char *argv[])
         printf("\nYour input file: %s \n", argv[1]);
     fp = fopen(argv[1], "r");
     fscanf(fp, "%d %d %d", &len_a, &len_b, &c_len);
-    string_A = (char *)malloc((len_a + 1) * sizeof(char *));
-    string_B = (char *)malloc((len_b + 1) * sizeof(char *));
-    unique_chars_C = (char *)malloc((c_len + 1) * sizeof(char *));
+    string_A = (char *)calloc((len_a + 1), sizeof(char *));
+    string_B = (char *)calloc((len_b + 1), sizeof(char *));
+    unique_chars_C = (char *)calloc((c_len + 1), sizeof(char *));
 
     fscanf(fp, "%s %s %s", string_A, string_B, unique_chars_C);
 
     chunk_size_p = (c_len / num_procs);
+    resto_p = (c_len % num_procs);
     chunk_size_dp = ((len_b + 1) / num_procs);
 
     if (my_rank == 0)
@@ -168,47 +169,28 @@ int main(int argc, char *argv[])
         printf("chunk_p: %d chunk_dp: %d procs: %d\n", chunk_size_p, chunk_size_dp, num_procs);
     }
 
-    DP_Results = (int *)malloc((len_b + 1) * sizeof(int));
-    dp_prev_row = (int *)malloc((len_b + 1) * sizeof(int));
+    atual = (int *)calloc((len_b + 1), sizeof(int));
+    anterior = (int *)calloc((len_b + 1), sizeof(int));
 
-    P_Matrix = (int *)malloc((c_len * (len_b + 1)) * sizeof(int));
-    P_Matrix = (int *)malloc((c_len * (len_b + 1)) * sizeof(int));
-    /*
-        if(my_rank ==0)
-    {
-       start_time =  MPI_Wtime();
-        printf("lcs is: %d\n",lcs(DP_Results,string_A,string_B,len_a,len_b));
-        stop_time = MPI_Wtime();
-        printf("time taken by normal algorithm is: %lf\n",stop_time-start_time);
-    }
+    P_Matrix = (int *)calloc((c_len * (len_b + 1)), sizeof(int));
 
-
-        for(int k=0;k<len_a+1;k++)
-        {
-            for(int l=0;l<len_b+1;l++)
-            {
-                DP_Results[k][l]=0;
-            }
-        }
-    */
     start_time_yang = MPI_Wtime();
 
-    calc_P_matrix_v2(P_Matrix, string_B, len_b, unique_chars_C, c_len, my_rank, chunk_size_p);
+    calc_matriz_dist(P_Matrix, string_B, len_b, unique_chars_C, c_len, my_rank, chunk_size_p, resto_p, num_procs);
 
-    res = lcs_yang_v2(DP_Results, dp_prev_row, P_Matrix, string_A, string_B, unique_chars_C, len_a, len_b, c_len, my_rank, chunk_size_dp);
+    res = calc_lcs(atual, anterior, P_Matrix, string_A, string_B, unique_chars_C, len_a, len_b, c_len, my_rank, chunk_size_dp);
 
     stop_time_yang = MPI_Wtime();
 
     if (my_rank == 0)
     {
-        printf("lcs_yang_v2 is: %d\n", res);
-        printf("time taken for lcs_yang_v2 is: %lf\n", stop_time_yang - start_time_yang);
+        printf("calc_lcs is: %d\n", res);
+        printf("time taken for calc_lcs is: %lf\n", stop_time_yang - start_time_yang);
     }
-    // deallocate pointers
-    free(P_Matrix);
-    free(DP_Results);
 
-    // Shutdown MPI (important - don't forget!)
+    free(P_Matrix);
+    free(atual);
+
     MPI_Finalize();
     return 0;
 }
