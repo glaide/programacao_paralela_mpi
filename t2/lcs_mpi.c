bb0aaea999b5a72b37dac26c0afa8cba1606519d
Glaide <glaide.oliveira@avenue.us>
2022-09-11 17:18:05 -0300

    update minha versao

--- t2/lcs_mpi.c
+++ t2/lcs_mpi.c
@@ -13,13 +13,13 @@ char *string_B;
 char *unique_chars_C; // unique alphabets
 int c_len;
 int *P_Matrix;
-int *atual; // to store the DP values
-int *anterior;
+int *DP_Results; // to store the DP values
+int *dp_prev_row;
 // function prototypes
 int get_index_of_character(char *str, char x, int len);
 void print_matrix(int **x, int row, int col);
-void calc_matriz_dist(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size, int resto_p, int num_procs);
-int calc_lcs(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size);
+void inicia_matriz_P(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk, int resto_chunk);
+int lcs_yang_v2(int *atual, int *anterior, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size, int resto);
 int lcs(int **DP, char *A, char *B, int m, int n);
 
 int get_index_of_character(char *str, char x, int len)
@@ -46,65 +46,124 @@ void print_matrix(int **x, int row, int col)
     }
 }
 
-void calc_matriz_dist(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size, int resto_p, int num_procs)
+void calc_P_matrix_v2(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk_size, int resto)
 {
-    char receive_array_for_scatter_c[chunk_size];
-    int receive_array_for_scatter_p[chunk_size * (len_b + 1)];
-    // Scatter the char array chunks by sending each process a particular chunk
-    MPI_Scatter(c, chunk_size, MPI_CHAR, &receive_array_for_scatter_c, chunk_size, MPI_CHAR, 0, MPI_COMM_WORLD);
-    // Scatter the char array chunks by sending each process a particular chunk
-    MPI_Scatter(P, chunk_size * (len_b + 1), MPI_INT, &receive_array_for_scatter_p, chunk_size * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
-    // Broadcast the whole b  array to everybody
+    char string_c_scatter[chunk_size];
+    int vetor_p_scatter[chunk_size * (len_b + 1)];
+
+    MPI_Scatter(c, chunk_size, MPI_CHAR, &string_c_scatter, chunk_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    MPI_Scatter(P, chunk_size * (len_b + 1), MPI_INT, &vetor_p_scatter, chunk_size * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
+
     MPI_Bcast(b, len_b, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    for (int i = 0; i < chunk_size; i++)
+    int i, j;
+    for (i = 0; i < chunk_size; i++)
     {
-        for (int j = 1; j < len_b + 1; j++)
+        for (j = 1; j < len_b + 1; j++)
         {
-            if (b[j - 1] == receive_array_for_scatter_c[i])
+            if (b[j - 1] == string_c_scatter[i])
             {
-                receive_array_for_scatter_p[(i * (len_b + 1)) + j] = j;
+                vetor_p_scatter[i * (len_b + 1) + j] = j;
             }
             else
             {
-                receive_array_for_scatter_p[(i * (len_b + 1)) + j] = receive_array_for_scatter_p[(i * (len_b + 1)) + j - 1];
+                vetor_p_scatter[i * (len_b + 1) + j] = vetor_p_scatter[i * (len_b + 1) + j - 1];
             }
         }
     }
-    // now gather all the calculated values of P matrix in process 0
-    MPI_Gather(receive_array_for_scatter_p, chunk_size * (len_b + 1), MPI_INT, P, chunk_size * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
-}
 
-int calc_lcs(int *DP, int *prev_row, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size)
-{
-    MPI_Bcast(P, (u * (n + 1)), MPI_INT, 0, MPI_COMM_WORLD);
-    for (int i = 1; i < m + 1; i++)
+    MPI_Gather(vetor_p_scatter, chunk_size * (len_b + 1), MPI_INT, P, chunk_size * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (myrank == 0)
     {
-        int c_i = get_index_of_character(C, A[i - 1], u);
-        int dp_i_receive[chunk_size];
-        MPI_Scatter(DP, chunk_size, MPI_INT, &dp_i_receive, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
-        int start_id = (myrank * chunk_size);
-        int end_id = (myrank * chunk_size) + chunk_size;
-
-        int t, s;
-
-        for (int j = start_id; j < end_id; j++)
+        for (i = len_c - resto; i < len_c; i++)
         {
-            if (j == start_id && myrank == 0)
-                j = j + 1;
-            t = (0 - P[(c_i * (n + 1)) + j]) < 0;
-            s = (0 - (prev_row[j] - (t * prev_row[P[(c_i * (n + 1)) + j] - 1])));
-            dp_i_receive[j - start_id] = ((t ^ 1) || (s ^ 0)) * (prev_row[j]) + (!((t ^ 1) || (s ^ 0))) * (prev_row[P[(c_i * (n + 1)) + j] - 1] + 1);
-        }
-        // now gather all the calculated values of P matrix in process 0
-        MPI_Allgather(dp_i_receive, chunk_size, MPI_INT, DP, chunk_size, MPI_INT, MPI_COMM_WORLD);
-
-        for (int j = 1; j < n + 1; j++)
-        {
-            prev_row[j] = DP[j];
+            for (j = 1; j < len_b + 1; j++)
+            {
+                if (b[j - 1] == c[i])
+                {
+                    P[i * (len_b + 1) + j] = j;
+                }
+                else
+                {
+                    P[i * (len_b + 1) + j] = P[i * (len_b + 1) + j - 1];
+                }
+            }
         }
     }
-    return DP[n];
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+int lcs_yang_v2(int *atual, int *anterior, int *P, char *A, char *B, char *C, int m, int n, int u, int myrank, int chunk_size, int resto)
+{
+    MPI_Bcast(P, (u * (n + 1)), MPI_INT, 0, MPI_COMM_WORLD);
+    int *temp;
+    int i;
+
+    int start_id = (myrank * chunk_size);
+    int end_id = (myrank * chunk_size) + chunk_size;
+
+    int dp_i_receive[chunk_size];
+
+    for (i = 1; i < m; i++)
+    {
+        int c = get_index_of_character(C, A[i - 1], u);
+        int p_c_j, j;
+
+        MPI_Scatter(atual, chunk_size, MPI_INT, dp_i_receive, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+        j = start_id;
+
+        if (j == 0)
+            j = 1;
+
+        for (; j < end_id; j++)
+        {
+            p_c_j = matrizP[c * (n + 1) + j];
+            if (p_c_j)
+            {
+                dp_i_receive[j - start_id] = max(linha_anterior[j], linha_anterior[p_c_j - 1] + 1);
+            }
+            else
+            {
+                dp_i_receive[j - start_id] = linha_anterior[j];
+            }
+        }
+
+        MPI_Allgather(dp_i_receive, chunk_size, MPI_INT, atual, chunk_size, MPI_INT, MPI_COMM_WORLD);
+
+        if (myrank == 0)
+        {
+            for (j = n + 1 - resto; j < n + 1; j++)
+            {
+                p_c_j = matrizP[c * (n + 1) + j];
+                if (p_c_j)
+                {
+                    linha_atual[j] = max(linha_anterior[j], linha_anterior[p_c_j - 1] + 1);
+                }
+                else
+                {
+
+                    linha_atual[j] = linha_anterior[j];
+                }
+            }
+        }
+
+        temp = atual;
+        atual = anterior;
+        anterior = temp;
+
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    // o algoritmo faz uma troca entre as linhas a mais que devia na última iteração, então a corrigimos
+    temp = atual;
+    atual = anterior;
+    anterior = temp;
+
+    return linha_atual[n];
 }
 
 int lcs(int **DP, char *A, char *B, int m, int n)
@@ -139,7 +198,7 @@ int main(int argc, char *argv[])
 
     int my_rank;
     int num_procs;
-    int chunk_size_p, chunk_size_dp, resto_p; // chunk_size for P matrix and DP matrix
+    int chunk_size_p, chunk_size_dp, resto_p, resto_dp; // chunk_size for P matrix and DP matrix
     int res;
 
     MPI_Init(&argc, &argv);
@@ -163,34 +222,36 @@ int main(int argc, char *argv[])
     chunk_size_p = (c_len / num_procs);
     resto_p = (c_len % num_procs);
     chunk_size_dp = ((len_b + 1) / num_procs);
+    resto_dp = ((len_b + 1) % num_procs);
 
     if (my_rank == 0)
     {
         printf("chunk_p: %d chunk_dp: %d procs: %d\n", chunk_size_p, chunk_size_dp, num_procs);
     }
 
-    atual = (int *)calloc((len_b + 1), sizeof(int));
-    anterior = (int *)calloc((len_b + 1), sizeof(int));
+    int *atual = (int *)calloc((len_b + 1), sizeof(int));
+    int *anterior = (int *)calloc((len_b + 1), sizeof(int));
 
-    P_Matrix = (int *)calloc((c_len * (len_b + 1)), sizeof(int));
+    P_Matrix = (int *)calloc((c_len, (len_b + 1)) * sizeof(int));
 
     start_time_yang = MPI_Wtime();
 
-    calc_matriz_dist(P_Matrix, string_B, len_b, unique_chars_C, c_len, my_rank, chunk_size_p, resto_p, num_procs);
+    calc_P_matrix_v2(P_Matrix, string_B, len_b, unique_chars_C, c_len, my_rank, chunk_size_p, resto_p);
 
-    res = calc_lcs(atual, anterior, P_Matrix, string_A, string_B, unique_chars_C, len_a, len_b, c_len, my_rank, chunk_size_dp);
+    res = lcs_yang_v2(atual, anterior, P_Matrix, string_A, string_B, unique_chars_C, len_a, len_b, c_len, my_rank, chunk_size_dp, resto_dp);
 
     stop_time_yang = MPI_Wtime();
 
     if (my_rank == 0)
     {
-        printf("calc_lcs is: %d\n", res);
-        printf("time taken for calc_lcs is: %lf\n", stop_time_yang - start_time_yang);
+        printf("lcs_yang_v2 is: %d\n", res);
+        printf("time taken for lcs_yang_v2 is: %lf\n", stop_time_yang - start_time_yang);
     }
-
+    // deallocate pointers
     free(P_Matrix);
-    free(atual);
+    free(DP_Results);
 
+    // Shutdown MPI (important - don't forget!)
     MPI_Finalize();
     return 0;
 }
---
+++ vinicius/lcs.c
@@ -0,0 +1,362 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <time.h>
+
+#ifndef max
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#define MAX_CHAR 256
+
+typedef unsigned short mtype;
+
+// #define DEBUGMATRIX
+
+/* Read sequence from a file to a char vector.
+ Filename is passed as parameter */
+
+char *read_seq(char *fname)
+{
+	// file pointer
+	FILE *fseq = NULL;
+	// sequence size
+	long size = 0;
+	// sequence pointer
+	char *seq = NULL;
+	// sequence index
+	int i = 0;
+
+	// open file
+	fseq = fopen(fname, "rt");
+	if (fseq == NULL)
+	{
+		printf("Error reading file %s\n", fname);
+		exit(1);
+	}
+
+	// find out sequence size to allocate memory afterwards
+	fseek(fseq, 0, SEEK_END);
+	size = ftell(fseq);
+	rewind(fseq);
+
+	// allocate memory (sequence)
+	seq = (char *)calloc(size + 1, sizeof(char));
+	if (seq == NULL)
+	{
+		printf("Erro allocating memory for sequence %s.\n", fname);
+		exit(1);
+	}
+
+	// read sequence from file
+	while (!feof(fseq))
+	{
+		seq[i] = fgetc(fseq);
+		if ((seq[i] != '\n') && (seq[i] != EOF))
+			i++;
+	}
+	// insert string terminator
+	seq[i] = '\0';
+
+	// close file
+	fclose(fseq);
+
+	// return sequence pointer
+	return seq;
+}
+
+/**
+ * @brief Cria a string que possui os caracteres únicos presentes nas strings de entrada seqA e seqB.
+ * Em vez de percorrer toda a string que está sendo criada a cada vez que temos um caracter candidato
+ * a ser incluso, é criado um vetor que possui 0s e 1s para dizer se um caracter está contido em ao
+ * menos uma das strings de entrada. Após isso, é criada uma string com as posições do vetor marcadas
+ * com 1.
+ * @param seqA String qualquer.
+ * @param seqB String qualquer.
+ *
+ * @return char* String de caracteres únicos.
+ */
+char *string_char_unicos(char *seqA, char *seqB)
+{
+	// Aloca um array com todos os possíveis chars que possamos ter de entrada (2⁸)
+	int *charsetC = calloc(MAX_CHAR, sizeof(int));
+	int len_c = 0;
+	// leio a seqB e separo seu chars únicos escrevendo 1 em sua posição de mem. equivalente em charsetC
+	for (int i = 0; i < strlen(seqA); i++)
+	{
+		if (charsetC[(int)seqA[i]] == 0)
+		{
+			len_c += 1; // contabiliza o num. de chars diferentes existentes
+		}
+		charsetC[(int)seqA[i]] = 1;
+	}
+	// faço o mesmo para a seqB
+	for (int i = 0; i < strlen(seqB); i++)
+	{
+		if (charsetC[(int)seqB[i]] == 0)
+		{
+			len_c += 1;
+		}
+		charsetC[(int)seqB[i]] = 1;
+	}
+
+	// aloco espaço para a string C e coloco seus valores (será sempre < 256)
+	char *seqC = calloc(len_c, sizeof(char));
+	int j = 0;
+	for (int i = 0; i < MAX_CHAR; i++)
+	{
+		if (charsetC[i] == 1)
+		{
+			seqC[j] = i;
+			j++;
+		}
+	}
+
+	return seqC;
+}
+
+int inidice_em_c(char *str_c, char chr)
+{
+	for (int i = 0; i < strlen(str_c); i++)
+	{
+		if (str_c[i] == chr)
+		{
+			return i;
+		}
+	}
+	// não encontra char na string c
+	return -1;
+}
+
+void inicia_matriz_P(int *P, char *b, int len_b, char *c, int len_c, int myrank, int chunk, int resto_chunk)
+{
+	char string_c_scatter[chunk];
+	int vetor_p_scatter[chunk * (len_b + 1)];
+	// Scatter the char array chunks by sending each process a particular chunk
+	MPI_Scatter(c, chunk, MPI_CHAR, &string_c_scatter, chunk, MPI_CHAR, 0, MPI_COMM_WORLD);
+	// Divide partes de P igualmente entre as threads
+	MPI_Scatter(P, chunk * (len_b + 1), MPI_INT, &vetor_p_scatter, chunk * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
+	// Compartilha a string B para todas as threads
+	MPI_Bcast(b, len_b, MPI_CHAR, 0, MPI_COMM_WORLD);
+	// parte paralela
+	int i, j;
+	for (i = 0; i < chunk; i++)
+	{
+		for (j = 1; j < len_b + 1; j++)
+		{
+			if (b[j - 1] == string_c_scatter[i])
+			{
+				vetor_p_scatter[i * (len_b + 1) + j] = j;
+			}
+			else
+			{
+				vetor_p_scatter[i * (len_b + 1) + j] = vetor_p_scatter[i * (len_b + 1) + j - 1];
+			}
+		}
+	}
+
+	// agrega todos os valores de P na thread 0
+	MPI_Gather(vetor_p_scatter, chunk * (len_b + 1), MPI_INT, P, chunk * (len_b + 1), MPI_INT, 0, MPI_COMM_WORLD);
+	// barreira para garantir que todas as threads chegaram até aqui
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	// parte que sobra para computar por último no processo 0
+	if (myrank == 0)
+	{
+		for (i = len_c - resto_chunk; i < len_c; i++)
+		{
+			for (j = 1; j < len_b + 1; j++)
+			{
+				if (b[j - 1] == c[i])
+				{
+					P[i * (len_b + 1) + j] = j;
+				}
+				else
+				{
+					P[i * (len_b + 1) + j] = P[i * (len_b + 1) + j - 1];
+				}
+			}
+		}
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+}
+
+/**
+ * @brief Algoritmo para cálculo do Problema da Maior Subsequência Comum utilizando como base o algoritmo
+ * desenvolvido por Yang et al. Durante a execução os vetores de entrada linha_atual e linha_anterior
+ * passam a ter novos valores escritos.
+ *
+ * @param linha_atual Vetor para armazenar os valores da linha anterior da matriz. Deve ter seus valores
+ * iniciais zerados.
+ * @param linha_anterior Vetor para armazenar os valores da linha atual da matriz. Deve ter seus valores
+ * iniciais zerados.
+ * @param seqA String A do algoritmo de Yang et al.
+ * @param seqB String B do algoritmo de Yang et al.
+ * @param seqC String C do algoritmo de Yang et al de caracteres únicos presentes em seqA e seqB.
+ * @param len_a Tamanho da string A.
+ * @param len_b Tamanho da string B.
+ * @param len_c Tamanho da string C.
+ * @param matrizP Matriz P do algoritmo de Yang et al armazenada em forma de array. Deve ser de tamanho
+ * len_c * (len_b + 1).
+ *
+ * @return int Tamanho da maior subsequência comum entre seqA e seqB.
+ */
+int calcula_lcs(int *linha_atual, int *linha_anterior, char *seqA, char *seqB, char *seqC,
+				int len_a, int len_b, int len_c, int *matrizP, int myrank, int chunk_size, int resto_chunk)
+{
+
+	// compartiha entre as threads a matriz P
+	MPI_Bcast(matrizP, (len_c * (len_b + 1)), MPI_INT, 0, MPI_COMM_WORLD);
+	int *temp;
+	int i;
+	// calcula qual parte esta thread deve fazer:
+	int start_id = (myrank * chunk_size);
+	int end_id = (myrank * chunk_size) + chunk_size;
+	// variável que cada thread terá p/ guardar a parte que será calculada
+	int dp_i_receive[chunk_size];
+	// O tamanho da matriz R seria de (len_a + 1) x (len_b + 1)
+	// Como utilizamos somente a linha atual e a linha anterior daquilo que seria a matriz R,
+	// não é necessário alocar ela inteira. Então só usamos estas 2 linhas
+
+	// quando i = 0, seu valor é sempre 0
+	for (i = 1; i < len_a; i++)
+	{
+		// c denota o índice do char A[i - 1] na string C.
+		int c = inidice_em_c(seqC, seqA[i - 1]);
+		int p_c_j, j;
+		// quando j = 0, seu valor é sempre 0
+		//  divide a tarefa entre as threads
+		MPI_Scatter(linha_atual, chunk_size, MPI_INT, dp_i_receive, chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+		// MPI_Barrier(MPI_COMM_WORLD);
+		j = start_id;
+		// não precisa calcular j = 0
+		if (j == 0)
+			j = 1;
+
+		for (; j < end_id; j++)
+		{
+			p_c_j = matrizP[c * (len_b + 1) + j];
+			if (p_c_j)
+			{
+				dp_i_receive[j - start_id] = max(linha_anterior[j], linha_anterior[p_c_j - 1] + 1);
+			}
+			else
+			{
+				dp_i_receive[j - start_id] = linha_anterior[j];
+			}
+		}
+		// Aqui é como se tivesse uma barreira, espera todas as threads acabarem de executar
+		// para eviar uma racing condition
+		MPI_Allgather(dp_i_receive, chunk_size, MPI_INT, linha_atual, chunk_size, MPI_INT, MPI_COMM_WORLD);
+
+		if (myrank == 0)
+		{
+			for (j = len_b + 1 - resto_chunk; j < len_b + 1; j++)
+			{
+				p_c_j = matrizP[c * (len_b + 1) + j];
+				if (p_c_j)
+				{
+					linha_atual[j] = max(linha_anterior[j], linha_anterior[p_c_j - 1] + 1);
+				}
+				else
+				{
+					// linha_atual[j] = max(linha_anterior[j], 0);
+					// mas como linha_anterior[j] >= 0, então basta pegar o da linha_anterior[j];
+					linha_atual[j] = linha_anterior[j];
+				}
+			}
+		}
+		// A linha anterior passa a ser a linha atual e vice-versa. Não tem porblema a linha anterior
+		//  ter os mesmos dados da anteriror pois serão sobre-escritos
+		temp = linha_atual;
+		linha_atual = linha_anterior;
+		linha_anterior = temp;
+		// espera todos os processos realizarem a troca das linhas para garantir a informação correta
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+	// o algoritmo faz uma troca entre as linhas a mais que devia na última iteração, então a corrigimos
+	temp = linha_atual;
+	linha_atual = linha_anterior;
+	linha_anterior = temp;
+
+	return linha_atual[len_b];
+}
+
+int main(int argc, char **argv)
+{
+	double start_time, midi_time, stop_time;
+
+	if (argc < 3)
+	{
+		printf("Não foram especificados arquivos de entrada.");
+		exit(1);
+	}
+
+	start_time = MPI_Wtime();
+	// ponteiros p/ as strings
+	char *seqA, *seqB, *seqC;
+	// tamanho das strings
+	int len_a, len_b, len_c;
+	// lê os arquivos especificados na entrada
+	seqA = read_seq(argv[1]);
+	seqB = read_seq(argv[2]);
+
+	len_a = strlen(seqA);
+	len_b = strlen(seqB);
+
+	// Criando string C
+	seqC = string_char_unicos(seqA, seqB);
+	if (!seqC)
+	{
+		printf("Erro ao alocar matriz string C");
+		return 1;
+	}
+	len_c = strlen(seqC);
+
+	int *linha_atual = calloc(len_b + 1, sizeof(int));
+	int *linha_anterior = calloc(len_b + 1, sizeof(int));
+	if (linha_atual == NULL || linha_anterior == NULL)
+	{
+		printf("Erro ao alocar linhas de R");
+		return 1;
+	}
+	// inicia a matriz P
+	int *matrizP = calloc(len_c * (len_b + 1), sizeof(int));
+	// int sizeP = len_c * (len_b + 1);
+	if (!matrizP)
+	{
+		printf("Erro ao alocar matriz P");
+		return 1;
+	}
+
+	int my_rank, num_procs; // chunk_size for P matrix and linha_atual matrix
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);   // grab this process's rank
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs); // grab the total num of processes
+
+	int chunk_size_p = (len_c / num_procs);
+	int resto_p = (len_c % num_procs);
+	int chunk_r = ((len_b + 1) / num_procs);
+	int resto_r = ((len_b + 1) % num_procs);
+
+	start_time = MPI_Wtime();
+
+	midi_time = MPI_Wtime();
+	inicia_matriz_P(matrizP, seqB, len_b, seqC, len_c, my_rank, chunk_size_p, resto_p);
+
+	int res = calcula_lcs(linha_atual, linha_anterior, seqA, seqB, seqC, len_a, len_b, len_c, matrizP, my_rank, chunk_r, resto_r);
+	stop_time = MPI_Wtime();
+	if (my_rank == 0)
+	{
+		printf("%d\n", res);
+		printf("%lf\n", stop_time - start_time);
+		printf("%lf\n", stop_time - midi_time);
+	}
+	free(linha_atual);
+	free(linha_anterior);
+	free(matrizP);
+
+	MPI_Finalize();
+	return 0;
+}
